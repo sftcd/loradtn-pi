f0fee7068cdf0bfd64d7b92caf136752bcebff3f
sftcd <stephen.farrell@cs.cd.ie>
2018-03-06 13:35:28 +0000

    bit of reorg

--- stable/pbmd.c
+++ stable/pbmd.c
@@ -1,11 +1,7 @@
 /*
     Handles logging of values - voltmeter, ammeter
     State machine controller
- 
- 
- 
 */
- 
  
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,22 +11,29 @@
 #include <phidget21.h>
 #include <syslog.h>
 
-#define BATTERY_LOG "/var/log/battery.log" 
-#define SNAP_LOG "/var/log/battery-snapshot.log" 
-#define BOOTFLAGS "/etc/bootflag" 
+#define BATTERY_LOG "/var/log/battery-new.log" 
+#define SNAP_LOG "/var/log/battery-snapshot-new.log" 
+#define BOOTFLAGS "/etc/bootflag-new" 
 
 #define SLEEP_STATE 0
 #define UP_STATE 1
+
+#define GREEDY_STR "GREEDY"
+#define MODERATE_STR "MODERATE"
 
 #define GREEDY_SLEEP 20
 #define MODERATE_SLEEP 18
 #define CONSERVATIVE_SLEEP 16
  
-char* batteryLogLocation, snapshotLocation, bootflags, mode;// = "/var/log/battery.log";
+char *batteryLogLocation, *snapshotLocation, bootflag, *mode;// = "/var/log/battery.log";
  
 int sleepDuration, sleepTime, wakeTime;
 
 FILE *logFile; 
+
+#ifndef min
+#define min(a,b) (a<b?a:b)
+#endif
 
 int updateLogfile(FILE *logfile,
 	int voltage,
@@ -40,53 +43,47 @@ int updateLogfile(FILE *logfile,
 	int spiking,
 	const char *progName);
 
+int updateSnapshotfile(const char *snapFileName, int voltage, int amps,  int state, int spiking);
+
 int clearSnapFile(char* snapFileName);
 
 char* getProgName(char* path)
 {
 	char* ret = strrchr( path, '/' );	   //returns null if '/' not found
-	
 	if( ret == NULL )
 		return path;
 	else
 		return ret + 1;
 }
  
-void init(int argc, char* argv[]){
-   
-    
-
+void init(int argc, char* argv[])
+{
     logFile = fopen( BATTERY_LOG, "a" );
     clearSnapFile(SNAP_LOG);
 }
  
-int getSleepTime(char* mode){
-
-    if(mode == "GREEDY"){
+int getSleepTime(char* mode)
+{
+    if(!strncmp(mode,GREEDY_STR,min(strlen(mode),strlen(GREEDY_STR)))){
         return GREEDY_SLEEP;
-    }
-    else if(mode == "MODERATE"){
+    } else if(!strncmp(mode,MODERATE_STR,min(strlen(mode),strlen(MODERATE_STR)))){
         return MODERATE_SLEEP;
     }
-        
     return CONSERVATIVE_SLEEP;
 }
  
  
- 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
     //parse args, init
     char *progName = getProgName(argv[0]);
     sleepDuration = (int)argv[1];
     mode = argv[2];
-
     
     init(argc, argv);
 
     //log
-    
     fputs("\nYYYY-M-MDD,HH:MM:SS,mVolts,mAmps,State,Spike\n",logFile);
-    
     
     int voltage = 0;
     int amps = 0;
@@ -100,6 +97,7 @@ int main(int argc, char* argv[]){
     //get voltage, amps, amps DUT
     
     while(1){
+
         updateLogfile(logFile, voltage, amps, ampsDUT, state, spiking, progName);
         updateSnapshotfile (SNAP_LOG, voltage, amps, state, spiking);
 
@@ -111,44 +109,32 @@ int main(int argc, char* argv[]){
             state = UP_STATE;
         }
 
-        //get current time
-        
-
+       //get current time
        time ( &rawTime );
 	   timeInfo = localtime ( &rawTime );
        sleepTime = getSleepTime(mode);
-       
 
-        //act based on state
-        if(state == SLEEP_STATE){
+       //act based on state
+       if(state == SLEEP_STATE){
             if(timeInfo->tm_hour >= sleepTime){
                 // sleep script
-            }
-
-            else{
-                if(mode == "GREEDY"){
+            } else {
+				if(!strncmp(mode,GREEDY_STR,min(strlen(mode),strlen(GREEDY_STR)))){
                     //sleep short amount of time
-                }
-                else if(mode == "MODERATE"){
+				} else if(!strncmp(mode,MODERATE_STR,min(strlen(mode),strlen(MODERATE_STR)))){
                     //sleep moderate time
-                }
-                else{
+                } else {
                     //sleep longer
                 }
             }
-            
-
-
             return 0;
         }
 
         //get voltage, amps, amps DUT
 
-
         //log
         updateLogfile(logFile, voltage, amps, ampsDUT, state, spiking, progName);
         updateSnapshotfile (SNAP_LOG, voltage, amps, state, spiking);
-        
         
         //sleepn
         sleep(sleepDuration);
@@ -156,22 +142,20 @@ int main(int argc, char* argv[]){
     
 }
 
-
 int clearSnapFile(char *snapFileName)
 {
 	FILE *filesnap = fopen( snapFileName, "w" );   
-	 
 	if (filesnap == 0) 
 	{
 		syslog ( LOG_ERR, "Could not open snapshot file: %s", snapFileName );
 		return -1;
 	}   
-	
 	fclose( filesnap ); // we need to overwrite it every time we write to it	
 	return 0;
 }
 
-char* getStateDesc(int state){
+char* getStateDesc(int state)
+{
     switch(state){
         case SLEEP_STATE:
             return "SLEEPING";
@@ -191,19 +175,14 @@ int updateSnapshotfile(const char *snapFileName, int voltage, int amps,  int sta
 		
 	time ( &rawtime );
 	timeinfo = localtime ( &rawtime );
-
 	strftime (timeStr, 80, "%Y-%m-%d,%H:%M:%S", timeinfo);
 
 	FILE *snapfile = fopen( snapFileName, "w" );			// Open file (delete previous)
-
-	if (snapfile == 0)		
-	{
+	if (snapfile == 0) {
 		syslog ( LOG_ERR, "Could not open snapshot file: %s", snapFileName );
 		return -1;	  
-	}
-	else 
-	{
-		snprintf( snapEntry, 200, "%s %dV%s %dmA %s\n", timeStr, voltage, spikingStr, amps, getStateDesc(state) );														   
+	} else {
+		snprintf( snapEntry, 200, "%s %dV%s %dmA %s\n", timeStr, voltage, spikingStr, amps, getStateDesc(state) );
 		fputs( snapEntry, snapfile );
 		fflush( snapfile );
 		fclose( snapfile );
@@ -222,20 +201,16 @@ int updateLogfile(FILE *logfile, int voltage, int amps, int dutAmps, int state, 
 	
 	time ( &rawTime );
 	timeInfo = localtime ( &rawTime );
-
 	strftime (timeStr, 80, "%Y-%m-%d,%H:%M:%S", timeInfo);
+
 	gethostname(hostName, 80);
 	
-	if (logfile == 0)
-	{
+	if (logfile == 0) {
 		syslog ( LOG_ERR, "Error: log file not open" );
 		return -1;
-	}
-	else
-	{
+	} else {
 		snprintf( logEntry, 200, "%s %s %s %d %d %s %s DUT: %d\n", 
 			timeStr, hostName, progName, voltage, amps, getStateDesc(state), spikingStr, dutAmps );
-
 		fputs( logEntry, logfile );
 		fflush( logfile );
 		return 0;
